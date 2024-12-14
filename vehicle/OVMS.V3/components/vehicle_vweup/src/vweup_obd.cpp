05366a36e75be7707130fbc255d4bc981b741f2a
zhargal <nams.zhar@gmail.com>
2024-12-14 17:42:23 +0100

    4

--- vehicle/OVMS.V3/components/vehicle_vweup/src/vweup_obd.cpp
+++ vehicle/OVMS.V3/components/vehicle_vweup/src/vweup_obd.cpp
@@ -198,7 +198,7 @@ void OvmsVehicleVWeUp::OBDInit()
     std::vector<int> sohArray = {125, 124, 123, 122, 121, 120, 119, 118, 117, 116};
     //SOHHistory -> sohArray;
     //int value = 125;
-    for(j = 0; j < ((vweup_modelyear > 2019) ? 14 : 17); j++){
+    for(i = 0; i < ((vweup_modelyear > 2019) ? 14 : 17); i++){
       SOHHistory->SetElemValue(i,sohArray);
 
     } 
