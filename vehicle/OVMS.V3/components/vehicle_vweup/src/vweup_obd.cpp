9b3a736df324354fd636105aa21de7604db53d53
zhargal <nams.zhar@gmail.com>
2025-02-18 02:53:05 +0100

    erasing vector's elements

--- vehicle/OVMS.V3/components/vehicle_vweup/src/vweup_obd.cpp
+++ vehicle/OVMS.V3/components/vehicle_vweup/src/vweup_obd.cpp
@@ -274,7 +274,7 @@ void OvmsVehicleVWeUp::OBDInit()
                                       
                                       7d  79  7b  78  75  76  78  79  74  74  76  75  74  72  74  71  73  71  73  70  70  6f  74  70  ff  ff  ff  ff  ff  ff  ff  ff  ff  ff  ff  ff  ff  ff  ff  ff};
                                       125,121,123,120,117,118,120,121,116,116,118,117,116,114,116,113,115,113,115,112,112,111,116,112,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255  */  
- /*   int i;
+    int i;
     //int j;
     int value;
     std::vector<int> sohVector = {125,122,125,122,119,119,120,122,117,117,118,118,118,115,118,114,117,114,113,114,114,112,117,115,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,255,
@@ -298,10 +298,12 @@ void OvmsVehicleVWeUp::OBDInit()
     for(i = 0; i < 680; i++){
         sohVector[i] = sohVector[i] *100 / 125;
         value = sohVector[i];
-        if (value == 204)
-            continue;
-        SOHHistory->SetElemValue(i, value);
-      }*/
+        if (value == 204){
+          value = sohVector.erase(value);
+        } else {
+          SOHHistory->SetElemValue(i, value);
+        }
+      }
 
 
     // Battery SOH:
@@ -1074,7 +1076,7 @@ void OvmsVehicleVWeUp::IncomingPollReply(const OvmsPoller::poll_job_t &job, uint
 
     case VWUP_BAT_MGMT_SOH_HIST: // (znams) Testing the reply from the PID 74CC
       
-      if (PollReply.FromUint8("VWUP_BAT_MGMT_SOH_HIST", value, 4)) {
+     /* if (PollReply.FromUint8("VWUP_BAT_MGMT_SOH_HIST", value, 4)) {
         int i;    //Number of cell pack
        // int j;    //Quarterly measurement
         int byteCounter = 256; // Starting position of the first byte
@@ -1095,7 +1097,7 @@ void OvmsVehicleVWeUp::IncomingPollReply(const OvmsPoller::poll_job_t &job, uint
           }
        // }
         ESP_LOGD(TAG, "SOH_history_from_74CC: %s", resultSoh.c_str());
-      }
+      }*/
       break; 
 
     case VWUP1_CHG_AC_U:
