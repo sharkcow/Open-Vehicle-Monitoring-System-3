ab91043bfd76868861d9501646751963c48ffbf9
zhargal <nams.zhar@gmail.com>
2025-03-26 13:13:36 +0100

    1

--- vehicle/OVMS.V3/components/vehicle_vweup/src/vweup_obd.cpp
+++ vehicle/OVMS.V3/components/vehicle_vweup/src/vweup_obd.cpp
@@ -324,9 +324,10 @@ void OvmsVehicleVWeUp::OBDInit()
                 sum += soh;
                 count++;
             }
+          SOHPerPackMax->SetElemValue(j, maxSOH);
+          SOHPerPackMin->SetElemValue(j, minSOH);
           } 
-        SOHPerPackMax->SetElemValue(j, maxSOH);
-        SOHPerPackMin->SetElemValue(j, minSOH);
+
         }
 
 
