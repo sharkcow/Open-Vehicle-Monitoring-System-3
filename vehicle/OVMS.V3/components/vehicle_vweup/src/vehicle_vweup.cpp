8beaf0b85442529db71a2c288b678a57b7e10fdf
zhargal <nams.zhar@gmail.com>
2025-07-29 16:23:39 +0200

    1

--- vehicle/OVMS.V3/components/vehicle_vweup/src/vehicle_vweup.cpp
+++ vehicle/OVMS.V3/components/vehicle_vweup/src/vehicle_vweup.cpp
@@ -750,13 +750,13 @@ void OvmsVehicleVWeUp::Ticker60(uint32_t ticker)
   std::time_t time_cast = static_cast<std::time_t>(TimeCurrent);
   std::tm* utc_tm = std::gmtime(&time_cast);
   int month = utc_tm->tm_mon + 1;
-  if (month == 1 || month == 4 || month == 7 || month == 10 && SohDataNotified == false && MyWebServer.m_client_cnt >= 1)
+  if ((month == 1 || month == 4 || month == 7 || month == 10) && SohDataNotified == false && MyWebServer.m_client_cnt >= 1)
   {
     if (m_autonotifications_VW) {
       NotifySohHistoryChange();
       SohDataNotified = true;
     }
-  } else if (month != 1 || month != 4 || month != 7 || month != 10)
+  } else if (month != 1 && month != 4 && month != 7 && month != 10)
     {
       SohDataNotified = false;
     }
