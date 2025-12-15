a8fd7488a977efa074b3bfbad86cd79badf3b49c
zhargal <nams.zhar@gmail.com>
2025-12-15 15:59:13 +0100

    Comment line

--- vehicle/OVMS.V3/components/vehicle_vweup/src/vweup_obd.cpp
+++ vehicle/OVMS.V3/components/vehicle_vweup/src/vweup_obd.cpp
@@ -218,6 +218,7 @@ void OvmsVehicleVWeUp::OBDInit()
     SOHVectorSizeFake = MyMetrics.InitInt("xvu.b.soh.vectorsize.fake", SM_STALE_NONE, 0); //(znams)
     SOHPerPackStdDevFake = MyMetrics.InitVector<float>("xvu.b.soh.standev.fake", SM_STALE_NONE, 0, Percentage); //(znams)
  
+    // Comment test line
     //int i;
     //int j;
     //int value;
